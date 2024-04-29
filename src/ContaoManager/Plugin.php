db1665fb7d2c8b44b953190c9a949bae3d657991
stefanperetzki <spe@ndcom.de>
2024-04-29 10:56:58 +0200

    update

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -16,7 +16,7 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Inndividuell\ContaoJobsExtension\InndividuellContaoTestimonialsBundle;
+use Inndividuell\ContaoJobsExtension\InndividuellContaoJobsBundle;
 use Symfony\Component\Config\Loader\LoaderResolverInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\RouteCollection;
@@ -26,7 +26,7 @@ class Plugin implements BundlePluginInterface
     public function getBundles(ParserInterface $parser): array
     {
         return [
-            BundleConfig::create(InndividuellContaoTestimonialsBundle::class)
+            BundleConfig::create(InndividuellContaoJobsBundle::class)
                 ->setLoadAfter([ContaoCoreBundle::class]),
         ];
     }
