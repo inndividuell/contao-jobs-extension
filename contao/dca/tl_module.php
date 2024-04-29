8a3a232d7d16b1e97f440f2361957da505d5fa5d
stefanperetzki <spe@ndcom.de>
2024-04-29 14:49:59 +0200

    update

--- contao/dca/tl_module.php
+++ contao/dca/tl_module.php
@@ -7,7 +7,7 @@ use Contao\Controller;
 use Contao\DataContainer;
 use Contao\System;
 
-$GLOBALS['TL_DCA']['tl_module']['palettes']['jobslist']         = '{title_legend},name,type;{config_legend},inn_jobslist_detail_page,inn_jobslist_items_new,inn_jobslist_items_new_show,inn_jobslist_sorting,inn_jobslist_limit,inn_jobslist_detail_button_text;{cta_legend:hide},inn_jobslist_cta_headline,inn_jobslist_cta_button_text,inn_jobslist_cta_button_url,inn_jobslist_cta_position,inn_jobslist_add_image;{expert_legend:hide},guests,cssID';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['jobslist']         = '{title_legend},name,type;{config_legend},inn_jobslist_detail_page,inn_jobslist_items_new,inn_jobslist_items_new_show,inn_jobslist_items_type_show,inn_jobslist_sorting,inn_jobslist_limit,inn_jobslist_detail_button_text;{cta_legend:hide},inn_jobslist_cta_headline,inn_jobslist_cta_button_text,inn_jobslist_cta_button_url,inn_jobslist_cta_position,inn_jobslist_add_image;{expert_legend:hide},guests,cssID';
 $GLOBALS['TL_DCA']['tl_module']['palettes']['jobsreader']         = '{title_legend},name,type;{config_legend},inn_jobsreader_duties_headline,inn_jobsreader_requirements_headline,inn_jobsreader_back_link_text,inn_jobsreader_back_link_page,inn_jobsreader_application_link,inn_jobsreader_email_link,inn_jobsreader_whatsapp_link;{expert_legend:hide},guests,cssID';
 
 $GLOBALS['TL_DCA']['tl_module']['fields']['inn_jobslist_cta_headline'] = array
@@ -117,6 +117,13 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['inn_jobslist_items_new'] = array
     'sql'                     => "text NULL"
 );
 $GLOBALS['TL_DCA']['tl_module']['fields']['inn_jobslist_items_new_show'] = array
+(
+    'exclude'                 => true,
+    'inputType'               => 'checkbox',
+    'eval'                    => array('mandatory'=>false),
+    'sql'                     => "char(1) NULL"
+);
+$GLOBALS['TL_DCA']['tl_module']['fields']['inn_jobslist_items_type_show'] = array
 (
     'exclude'                 => true,
     'inputType'               => 'checkbox',
